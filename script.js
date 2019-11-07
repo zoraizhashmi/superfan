1b30d421ec8a7a9779f409120049238be74a4f8c
zoraizhashmi <37843506+zoraizhashmi@users.noreply.github.com>
2019-11-07 15:01:00 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// use variables to simplify your life!
+var a = document.getElementById("img1");
+var b = document.getElementById("img2");
+var c = document.getElementById("img3");
+var d = document.getElementById("img4");
+var e = document.getElementById("img5");
+var f = document.getElementById("img6");
+var g = document.getElementById("img7");
+var h = document.getElementById("img8");
+var main = document.getElementById("mainatt");
+
+rand = Math.floor(Math.random() * 3);
+setMainImg()
+
+function setMainImg(){
+	if (rand == 0) {
+		main.src = "images/starter1.jpg";
+	} 
+	else if ( rand == 1){
+		main.src = "images/starter2.jpg";
+	}
+	else if ( rand == 2) {
+		main.src = "images/starter3.jpg";
+	}
+}
